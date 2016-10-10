9562e286cde03b6dbe27ef79eccc308f537916f5
Matthias Wenz <matwen@microsoft.com>
2016-10-10 16:55:16 +0200

    Add button to crash app

--- app/src/main/java/de/bowstreet/testandroidapp/MainActivity.java
+++ app/src/main/java/de/bowstreet/testandroidapp/MainActivity.java
@@ -2,12 +2,24 @@ package de.bowstreet.testandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                throw new RuntimeException("Test exception");
+            }
+        });
     }
 }
