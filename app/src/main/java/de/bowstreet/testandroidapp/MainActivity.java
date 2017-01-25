5b2c608292ce837b41475d1e611f234b979ca5af
Matthias Wenz <matwen@microsoft.com>
2017-01-25 11:20:53 +0100

    Add MobileCenter SDK

--- app/src/main/java/de/bowstreet/testandroidapp/MainActivity.java
+++ app/src/main/java/de/bowstreet/testandroidapp/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
@@ -13,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileCenter.start(getApplication(), "2f85b1e5-f98d-4d7f-95f0-3509876fa2dd", Analytics.class, Crashes.class);
+
 
         mButton = (Button) findViewById(R.id.button);
         mButton.setOnClickListener(new View.OnClickListener() {
