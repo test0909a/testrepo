c8b435b74c3b61b4039670a4e6778c27be4a9e4b
Dennis Pan <dpan@microsoft.com>
2017-06-27 13:58:34 -0700

    Initial project with hellow world button

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Dennis Pan on 6/27/17.
+//  Copyright © 2017 Dennis Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
