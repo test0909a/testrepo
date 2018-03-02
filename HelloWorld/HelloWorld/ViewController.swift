878bb2614fc92ad2cfceba85bf1d6690ac7990ee
Dennis Pan <dpan@microsoft.com>
2018-03-02 12:41:20 -0800

    Fix build break

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoadss()
+        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
