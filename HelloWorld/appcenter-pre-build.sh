2b0735062542f8ac443be787c330abf0f91d2266
Dennis Pan <dpan@microsoft.com>
2018-03-06 16:34:56 -0800

    update script

--- HelloWorld/appcenter-pre-build.sh
+++ HelloWorld/appcenter-pre-build.sh
@@ -2,11 +2,11 @@ echo Running pre build
 export
 
 echo Sleeping
-sleep 5 * 60
+sleep 300
 echo Slept 5 minutes
-sleep 5 * 60
+sleep 300
 echo Slept 10 minutes
-sleep 5 * 60
+sleep 300
 echo Slept 15 minutes
-sleep 5 * 60
+sleep 300
 echo Slept 20 minutes
