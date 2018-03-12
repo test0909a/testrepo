d94def3d5909fc70c4f8e2ce6362a714a98e3117
Dennis Pan <dpan@microsoft.com>
2018-03-12 14:33:57 -0700

    Update comments

--- HelloWorld/appcenter-pre-build.sh
+++ HelloWorld/appcenter-pre-build.sh
@@ -5,7 +5,7 @@ echo Sleeping
 sleep 300
 echo Slept 5 minutes. Sleeping 5 more minutes
 sleep 300
-echo Slept 10 minutes
+echo Slept 10 minutes. Sleeping 5 more minutes
 sleep 300
 echo Slept 15 minutes
 sleep 300
