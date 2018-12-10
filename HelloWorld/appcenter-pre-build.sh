053c4733db0dd7491a0a3852b57b3436e997408c
Dennis Pan <dpan@microsoft.com>
2018-12-10 12:05:05 -0800

    remove sleep

--- HelloWorld/appcenter-pre-build.sh
+++ HelloWorld/appcenter-pre-build.sh
@@ -1,12 +1,2 @@
 echo Running pre build
 export
-
-echo Sleeping
-sleep 300
-echo Slept 5 minutes
-sleep 300
-echo Slept 10 minutes
-sleep 300
-echo Slept 15 minutes
-sleep 300
-echo Slept 20 minutes
